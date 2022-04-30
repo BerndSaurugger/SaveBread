647e070319ec47390e96aadee01807a555b70f1f
Bernd Saurugger <bernd.saurugger@tuwien.ac.at>
2022-04-30 15:13:34 +0000

    feature: add file input for xlsx and csv files

--- src/filehandler/input.py
+++ src/filehandler/input.py
@@ -1,9 +1,12 @@
-from pandas import read_excel
+from pandas import read_excel, read_csv
 
 
 def file_input(file: str):
-    assert file.endswith(('.xlsx','.xls')), "Filetype must be xlsx"
-    df_file = read_excel(file, sheet_name=0)
+    assert file.endswith(('.xlsx','.xls', '.csv')), "Filetype must be xlsx or csv"
+    if file.endswith('.csv'):
+        df_file = read_csv(file)
+    else:
+        df_file = read_excel(file, sheet_name=0)
     return df_file
 
-print(file_input("src/data/feature_list.xlsx"))
+# print(file_input("src/data/Bakery.csv "))
