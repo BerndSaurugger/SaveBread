39ee5f81a250bc4f4489ed593c825a1885dd6f9d
moberleitner <matthias.oberleitner@redbull.com>
2022-05-24 21:19:27 +0200

    refactor streamlig app

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -10,31 +10,28 @@ import logging
 st.set_page_config(layout="wide")
 
 
-@st.cache
-def first_execution_date():
-    """ Used so that reloading the web app doesn't create a new log file every time """
-    return datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
+def main():
+    col1, col2 = st.columns(2)
+    today = '2020-03-01'
+
+    dummy_data = train_model.get_data_with_predictions_from_dummy_data()
+    with col1:
+        st.text('Data Frame 1')
+        articles_per_timeframe(dummy_data, today)
+
+        st.text('Data Frame 2')
+        st.dataframe(dummy_data)
+
+    with col2:
+        st.text('Data Frame 3')
+        st.dataframe(dummy_data)
+
+        st.text('Data Frame 4')
+        st.dataframe(dummy_data)
 
 
-logger = logging.getLogger()
-formatter = logging.Formatter('%(asctime)s | %(name)s | %(levelname)s | %(message)s')
-
-file_handler = logging.FileHandler(
-    f'src/logs/savebread-{first_execution_date()}.log'
-)
-file_handler.setLevel(logging.DEBUG)
-file_handler.setFormatter(formatter)
-
-stream_handler = logging.StreamHandler()
-stream_handler.setFormatter(formatter)
-
-logger.addHandler(file_handler)
-logger.addHandler(stream_handler)
-
-
-def articles_per_timeframe():
-    st.text('Data Frame 1')
-    df_after_today = dummy_data[dummy_data.date > today]
+def articles_per_timeframe(data, today):
+    df_after_today = data[data.date > today]
 
     time_window = st.selectbox('Which timeframe do you want to show', (
         "Tomorrow",
@@ -55,18 +52,30 @@ def articles_per_timeframe():
     st.dataframe(df)
 
 
-col1, col2 = st.columns(2)
-today = '2020-03-01'
+@st.cache
+def first_execution_date():
+    """ Used so that reloading the web app doesn't create a new log file every time """
+    return datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
 
-dummy_data = train_model.get_data_with_predictions_from_dummy_data()
-with col1:
-    articles_per_timeframe()
 
-    st.text('Data Frame 2')
-    st.dataframe(dummy_data)
+def setup_logger():
+    logger = logging.getLogger()
+    formatter = logging.Formatter('%(asctime)s | %(name)s | %(levelname)s | %(message)s')
 
-with col2:
-    st.text('Data Frame 3')
-    st.dataframe(dummy_data)
-    st.text('Data Frame 4')
-    st.dataframe(dummy_data)
+    file_handler = logging.FileHandler(
+        f'src/logs/savebread-{first_execution_date()}.log'
+    )
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(formatter)
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
+    return logger
+
+
+if __name__ == '__main__':
+    logger = setup_logger()
+    main()
