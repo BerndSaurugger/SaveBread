b0b49255bec309d2376970c1c454a6a0f37f013a
Renato Fonseca <renato.fonseca@outlook.de>
2022-05-26 14:16:45 +0200

    feat global selector and st.columns changed

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -1,7 +1,12 @@
-# Start with
-# streamlit run ./src/streamlit_app.py
+"""
+ Start with
+streamlit run ./src/streamlit_app.py
+ """
+from unicodedata import decimal
+from matplotlib.pyplot import axis
 import streamlit as st
-import models.train_model as train_model
+import pandas as pd
+import models.train_model as tm #train_model
 import numpy as np
 import datetime
 import logging
@@ -11,45 +16,61 @@ st.set_page_config(layout="wide")
 
 
 def main():
-    col1, col2 = st.columns(2)
     today = '2020-03-01'
-
-    dummy_data = train_model.get_data_with_predictions_from_dummy_data()
-    with col1:
-        st.text('Data Frame 1')
-        articles_per_timeframe(dummy_data, today)
-
-        st.text('Data Frame 2')
-        st.dataframe(dummy_data)
-
-    with col2:
-        st.text('Data Frame 3')
-        st.dataframe(dummy_data)
-
-        st.text('Data Frame 4')
-        st.dataframe(dummy_data)
-
-
-def articles_per_timeframe(data, today):
-    df_after_today = data[data.date > today]
-
+    dummy_data = tm.get_data_with_predictions_from_dummy_data()
+    
+    
     time_window = st.selectbox('Which timeframe do you want to show', (
         "Tomorrow",
         "Next Week",
         "Next 4 Weeks"
     ))
+    
+    col1, col2 = st.columns([3,5])
+    
+    with col1:
+        st.header("Aggregated Predictions")
+        #st.text('')
+        df_input = articles_per_timeframe(dummy_data, today,time_window)
+        st.dataframe(data= df_input,
+                        width=None)
+        
+
+        st.text('Visualization 1')
+        #df = articles_per_timeframe(dummy_data, today)
+        #st.bar_chart(df)
+        st.dataframe(data= df_input,
+                        width=None)
+
+    with col2:
+        st.header("Week predictions")
+        #st.text('Data Frame 3')
+        st.dataframe(dummy_data.round())
+
+        st.text('Visualization 2')
+        st.dataframe(dummy_data)
+
+
+def articles_per_timeframe(data, today, tw):
+    """
+    TODO: Docstring
+    """
+    df_after_today = data[data.date > today]
+    time_window = tw
+
     if time_window == "Tomorrow":
         filtered_df = df_after_today[df_after_today.date == '2020-03-02']
     elif time_window == 'Next Week':
         filtered_df = df_after_today[df_after_today.date <= '2020-03-09']
     elif time_window == 'Next 4 Weeks':
         filtered_df = df_after_today[df_after_today.date < '2020-03-30']
-
-    # filtered_df = filtered_df.sum()
-    df = filtered_df.drop(['date', 'daytime', 'weekday', 'holiday', 'h_type', 'weather', 'temp'], axis=1)
-    df = df.sum().T
-    df = df.apply(np.ceil)
-    st.dataframe(df)
+    df = filtered_df.drop(['date', 'daytime', 'weekday', 'holiday', 'h_type', 'weather', 'temp'],axis=1)
+    df = df.transpose()
+    df = df.sum(axis=1).round(decimals=0)
+    df = df.reset_index(level=0)
+    df.rename( columns = {'index':'products'},inplace = True)
+    df.columns = ['products','amount']
+    return df
 
 
 @st.cache
--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -1,6 +1,10 @@
-# Streamlit operations
+"""
+This module allows to encapsulate visualization methods. This visualization methods
+are solely used in the UI of the savebread project.
+"""
 import streamlit as st
-
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Table View
 def table_viewer(dataframe):
@@ -10,6 +14,21 @@ def table_viewer(dataframe):
     :param dataframe: dataframe with result predictions
     """
     if dataframe.empty:
-        st.text("No predictions avaiable.")
+        st.text("No predictions available.")
     else:
         st.dataframe(data=dataframe)
+
+def line_chart(dataframe):
+    """
+    TODO: Docstring
+    """
+  
+
+
+def histogram(dataframe):
+    """
+    TODO: Docstring
+    """
+    pass
+
+
