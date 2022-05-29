1dcbbf638df55bf3613193bce62cbe46b7cac018
Renato Fonseca <renato.fonseca@outlook.de>
2022-05-30 00:22:14 +0200

    new filtering

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -4,8 +4,10 @@
  """
 import datetime
 import logging
+from matplotlib.pyplot import axis
 import streamlit as st
 import models.train_model as tm
+import numpy as np
 # import visualization.visualize as vis
 
 
@@ -20,8 +22,7 @@ def main():
     dummy_data = tm.get_data_with_predictions_from_dummy_data()
     time_window = st.selectbox('Which timeframe do you want to show', (
         "Tomorrow",
-        "Next Week",
-        "Next 4 Weeks"
+        "Next Week"
     ))
 
     col1, col2 = st.columns([3, 5])
@@ -32,15 +33,28 @@ def main():
         st.text('Visualization 1')
         # df_inp_vis = articles_per_timeframe(dummy_data, today, time_window)
         # visualization.line_chart(df_inp_vis)
-        st.dataframe(data=df_input, width=None)
+        # st.dataframe(data=df_input, width=None)
 
     with col2:
-        st.header("Week predictions")
+       #  st.header("Week predictions")
         df_input = articles_per_timeframe(dummy_data, today, time_window, False)
-        st.dataframe(data=df_input, width=None)
-        st.text('Visualization 2')
-        st.dataframe(data=df_input, width=None)
-
+        if time_window == "Tomorrow":
+            st.header("predictions for tomorrow")
+            df_daytime = df_input.drop(['date', 'weekday', 'holiday', 'h_type', 'weather', 'temp'], axis=1)
+            df_daytime.set_index('daytime', inplace=True)
+            st.dataframe(data=df_daytime.transpose(), width=None)
+            st.text('Visualization 2')
+           # st.bar_chart(data=df_daytime,height=550)
+        elif time_window == 'Next Week':
+            st.header("predictions for next week")
+            df_week = df_input.drop(['date', 'daytime', 'holiday', 'h_type', 'weather', 'temp'], axis=1)
+            df_week = df_week.apply(np.floor)
+            df_week = df_week.groupby('weekday').sum()
+            st.dataframe(data=df_week.transpose(), width=None)
+            # st.dataframe(data=df_week.transpose().apply(np.ceil), width=None)
+            st.text('Visualization 2')
+            # st.bar_chart(data=df_week,height=550)
+        
 
 def articles_per_timeframe(data, today, tw, agg=True):
     """
@@ -50,22 +64,19 @@ def articles_per_timeframe(data, today, tw, agg=True):
     time_window = tw
 
     if time_window == "Tomorrow":
-        # give me only amount of products for Daytime
         filtered_df = df_after_today[df_after_today.date == '2020-03-02']
     elif time_window == 'Next Week':
-        # give me only the amount of products for weekdays
         filtered_df = df_after_today[df_after_today.date <= '2020-03-09']
-    elif time_window == 'Next 4 Weeks':
-        filtered_df = df_after_today[df_after_today.date < '2020-03-30']
     if agg:
         df = filtered_df.drop(['date', 'daytime', 'weekday', 'holiday', 'h_type', 'weather', 'temp'], axis=1)
         df = df.transpose()
-        df = df.sum(axis=1).round(decimals=0)
+        df = df.apply(np.floor)
+        df = df.sum(axis=1)
         df = df.reset_index(level=0)
         df.rename(columns={'index': 'products'}, inplace=True)
         df.columns = ['products', 'amount']
     else:
-        df = filtered_df.round()
+        df = filtered_df
     return df
 
 
