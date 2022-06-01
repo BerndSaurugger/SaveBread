da46b723812ba48c27f06aed5021a64e124b96b7
Lena <lena-kl@gmx.de>
2022-06-01 20:38:46 +0000

    adding graphs

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -8,7 +8,9 @@ import logging
 import streamlit as st
 import models.train_model as tm
 import numpy as np
-# import visualization.visualize as vis
+import altair as alt
+import visualization.visualize as vis
+import pandas as pd
 
 
 st.set_page_config(layout="wide")
@@ -16,7 +18,9 @@ st.set_page_config(layout="wide")
 
 def main():
     """
-    TODO: Docstring
+    Inserts a dropdown menu to select period of interest: Tomorrow / Next week
+    Shows 2 tables with products and sales per period
+    Shows 2 graphs with products and sales per period
     """
     today = '2020-03-01'
     dummy_data = tm.get_data_with_predictions_from_dummy_data()
@@ -30,10 +34,12 @@ def main():
         st.header("Aggregated Predictions")
         df_input = articles_per_timeframe(dummy_data, today, time_window, True)
         st.dataframe(data=df_input, width=None)
-        st.text('Visualization 1')
-        # df_inp_vis = articles_per_timeframe(dummy_data, today, time_window)
-        # visualization.line_chart(df_inp_vis)
-        # st.dataframe(data=df_input, width=None)
+        st.text('Visualization Descending')
+        df_input_vis = articles_per_timeframe(dummy_data, today, time_window)
+        bars = alt.Chart(df_input_vis).mark_bar().encode(x='amount:Q',y=alt.Y('products:O', sort='-x'))
+        text = bars.mark_text(color='white').encode(text = 'amount:Q')
+        rule = alt.Chart(df_input_vis).mark_rule(color = 'red').encode(x='mean(amount):Q')
+        st.altair_chart(bars+text+rule, use_container_width=True)
 
     with col2:
         df_input = articles_per_timeframe(dummy_data, today, time_window, False)
@@ -44,22 +50,54 @@ def main():
             df_daytime = df_daytime.apply(np.floor)
             df_daytime = df_daytime.transpose().reset_index(level=0)
             df_daytime.rename(columns={'index': 'products', 1: 'morning', 2: 'afternoon'}, inplace=True)
+            df_daytime['sum'] = df_daytime['morning'] + df_daytime['afternoon']
             st.dataframe(data=df_daytime, width=None)
-            st.text('Visualization 2')
+            # Add Chart for aggregated view 
+            st.text('Visualization Tomorrow')
+            bars = alt.Chart(df_daytime).transform_fold(
+                ['morning', 'afternoon'],
+                as_=['daytime', 'sum_of_sales']).mark_bar().encode(
+                    x=alt.X('sum_of_sales:Q',
+                        stack = 'zero',
+                        scale= alt.Scale(domain=(0, 15), clamp= True)),
+                    y=alt.Y('products:O', sort = '-x'),
+                    color='daytime:N').interactive()
+            st.altair_chart(bars, use_container_width=True)
+
         elif time_window == 'Next Week':
             st.header("predictions for next week")
             df_week = df_input.drop(['date', 'daytime', 'holiday', 'h_type', 'weather', 'temp'], axis=1)
             df_week = df_week.apply(np.floor)
             df_week = df_week.groupby('weekday').sum()
             df_week = df_week.transpose().reset_index(level=0)
-            df_week.rename(columns={'index': 'products'}, inplace=True)
+            df_week.rename(columns={'index': 'products',
+            0.0: 'Monday', 
+            1.0: 'Tuesday',
+            2.0: 'Wednesday', 
+            3.0: 'Thursday', 
+            4.0: 'Friday', 
+            5.0: 'Saturday', 
+            6.0: 'Sunday'}, inplace=True)
+            df_week['sum']= df_week.iloc[:,1:].sum(axis=1)
             st.dataframe(data=df_week, width=None)
-            st.text('Visualization 2')
+            # Add Chart for aggregated view 
+            st.text('Visualization next week')
+            weekday_order = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+            bars = alt.Chart(df_week).transform_fold([
+                'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+              as_=['weekday', 'sum_of_sales']).mark_bar().encode(
+                  x=alt.X('sum_of_sales:Q',
+                  stack = 'zero',
+                  sort= weekday_order,
+                  scale= alt.Scale(domain=(0, 100), clamp= True)),
+                  y=alt.Y('products:O', sort= '-x'),
+                  color= alt.Color('weekday:N', sort= weekday_order)).interactive()
+            st.altair_chart(bars, use_container_width=True)
 
 
 def articles_per_timeframe(data, today, tw, agg=True):
     """
-    TODO: Docstring
+    Shows a df with products and sales per selected period: Tomorrow / Next Week
     """
     df_after_today = data[data.date > today]
     time_window = tw
--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -16,14 +16,36 @@ def table_viewer(dataframe):
     else:
         st.dataframe(data=dataframe)
 
+def line_chart(df_input_vis):
+    """
+    Table creates a simple chart displaying products & sales 
+    using input data from a dataframe.
+    :param dataframe: dataframe with result predictions
+    """
+    if dataframe.empty:
+        st.text("No predictions available.")
+    else:
+        st.dataframe(data=df_input_vis) 
 
-def line_chart(dataframe):
+def bar_chart_day(df_daytime):
     """
-    TODO: Docstring
+    Creates a simple bar chart displaying sales predictions per tomorrow
+    using input data from dataframe.
+    :param dataframe: dataframe with result predictions
     """
+    if dataframe.empty:
+        st.text("No predictions available.")
+    else:
+        st.dataframe(data=df_daytime)  
 
+def bar_chart_week(df_week):
+    """
+    Creates a simple bar chart displaying sales predictions per next week
+    using input data from dataframe.
+    :param dataframe: dataframe with result predictions
+    """
+    if dataframe.empty:
+        st.text("No predictions available.")
+    else:
+        st.dataframe(data=df_week) 
 
-def pie_chart(dataframe):
-    """
-    TODO: Docstring
-    """
